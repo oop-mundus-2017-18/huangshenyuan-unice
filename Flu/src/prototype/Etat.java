ba02a0644d319f7864b19be24841e4d0e571db7f
Shenyuan Huang <shenyuan.huang@etu.unice.fr>
2018-03-30 02:41:38 +0200

    neaten

--- Flu/src/prototype/Etat.java
+++
@@ -1,5 +0,0 @@
-package prototype;
-
-public enum Etat {
-	SICK, CONTAGIOUS,  
-}
--- Flu/src/prototype/Virus.java
+++
@@ -1,9 +0,0 @@
-package prototype;
-
-/**
- * @author HUANG Shenyuan
- * @date 2018-02-23 08:45
- * @email shenyuan.huang@etu.unice.fr
- */
-public interface Virus {
-}
